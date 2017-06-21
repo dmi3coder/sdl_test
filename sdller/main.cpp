53cd55b6207e3c8ad50e78f8eaaa74d95ca44cb8
dmi3coder <dmi3coder@gmail.com>
2017-06-21 09:22:33 +0300

    Byte texture drawing

--- sdller/main.cpp
+++ sdller/main.cpp
@@ -16,6 +16,23 @@ char *composition;
 Sint32 cursor;
 Sint32 selection_len;
 
+void putPixel(SDL_Surface *surface, int x, int y,Uint32 pixel){
+    int bytesPerPixel = surface->format->BytesPerPixel;
+    Uint8 *newPixelPosition = (Uint8 *)surface->pixels + y * surface->pitch + x * bytesPerPixel;
+    switch (bytesPerPixel) {
+        case 1:
+            *newPixelPosition = pixel;
+            break;
+        case 2:
+            *(Uint16 *)newPixelPosition = pixel;
+        case 4:
+            *(Uint32 *)newPixelPosition = pixel;
+        default:
+            break;
+    }
+    
+}
+
 int main(int argc, const char * argv[]) {
     SDL_Window* window;
     
@@ -23,25 +40,23 @@ int main(int argc, const char * argv[]) {
     
     window = SDL_CreateWindow("SDL testik", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
     
+    
     if(window == NULL){
         return -1;
     }
     SDL_Renderer* renderer = NULL;
     
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Texture * texture = SDL_CreateTexture(renderer,
+                                              SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 640, 480);
+    Uint32 * pixels = new Uint32[640 * 480];
     
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    SDL_RenderPresent(renderer);
-    
-    double x = 0;
-    double y = 50;
-    DeltaLooper deltaLooper;
-    
+    memset(pixels, 255, 640 * 480 * sizeof(Uint32));
+    bool leftMouseButtonDown = false;
     while (true)
     {
-            deltaLooper.getDeltaTime();
+            SDL_UpdateTexture(texture, NULL, pixels, 640 * sizeof(Uint32));
+        
             SDL_Event event;
             if(SDL_PollEvent(&event)){
                 switch (event.type) {
@@ -49,53 +64,31 @@ int main(int argc, const char * argv[]) {
                         SDL_DestroyWindow(window);
                         SDL_Quit();
                         return 0;
-                    case SDL_KEYDOWN:
-                        switch (event.key.keysym.scancode) {
-                            case 79:
-                                x += 10;
-                                break;
-                            case 80:
-                                x -= 10;
-                                break;
-                            case 81:
-                                y += 10;
-                                break;
-                            case 82:
-                                y -= 10;
-                                
-                            default:
-                                break;
+                    case SDL_MOUSEBUTTONUP:
+                        if (event.button.button == SDL_BUTTON_LEFT)
+                            leftMouseButtonDown = false;
+                        break;
+                    case SDL_MOUSEBUTTONDOWN:
+                        if (event.button.button == SDL_BUTTON_LEFT)
+                            leftMouseButtonDown = true;
+                    case SDL_MOUSEMOTION:
+                        if (leftMouseButtonDown)
+                        {
+                            int mouseX = event.motion.x;
+                            int mouseY = event.motion.y;
+                            for (int i = mouseY*640+mouseX -5 ; i < mouseY*640+mouseX + 5; i++) {
+                                pixels[i] = 0x34cc85;
+                            }
+                            
+                            
+                            
                         }
-                        std::cout << event.key.keysym.scancode << std::endl;
-                        break;
-                    case SDL_TEXTEDITING:
-                        composition = event.text.text;
-                        cursor = event.edit.start;
-                        selection_len = event.edit.length;
-                        break;
-                        
-                    default:
                         break;
                 }
-        }
-
-        SDL_Rect rect;
-        rect.x = x;
-        rect.y = y;
-        rect.w = 50;
-        rect.h = 50;
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-        
-        SDL_Texture *img = NULL;
-        SDL_Surface* loadedSurface = IMG_Load( "./image.png" );
-        img = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-        SDL_RenderCopy(renderer, img, NULL, &rect);
-        
-        SDL_RenderPresent(renderer);
-
     
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+            }
     }
 }
